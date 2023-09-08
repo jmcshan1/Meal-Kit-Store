c39e5b8a099a7529bf67928688b437ff5f8fec35
kbliss86 <132328278+kbliss86@users.noreply.github.com>
2023-09-08 01:30:31 -0500

    added model files for the SQL tables we need for the back end

---
+++ models/Order.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Order extends Model {}
+
+Order.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true,
+        },
+        order_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        product: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'product',
+                key: 'id',
+            }
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        city: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        zip: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        paymentType: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'order',
+    }
+);
+
+module.exports = Order;
---
+++ models/Style.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Style extends Model {}
+
+Style.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true,
+        },
+        style_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'style',
+    }
+);
+
+module.exports = Style;
