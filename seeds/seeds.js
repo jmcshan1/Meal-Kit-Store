ebd8cd70aedbe8c4ce1f560f5a9ef29ede0493bd
jmcshan1 <jmcshan01@gmail.com>
2023-09-11 18:36:03 -0500

    started developing view part of project

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/connection');
 const productSeed = require('./productSeed');
-const protientSeed = require('./protientSeed');
+const protientSeed = require('./protienSeed');
 const userSeed = require('./userSeed');
 const styleSeed = require('./styleSeed');
 
