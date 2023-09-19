11043fbe54869d4865f7cb705a640d73fd682788
jmcshan1 <jmcshan01@gmail.com>
2023-09-18 19:32:14 -0500

    fixed sign up bug, session is logged in now when user signs up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,9 @@ const { User } = require('../../models');
 router.post('/login', async (req, res) => {
   console.log('login route hit');
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
 
     if (!userData) {
       console.log('no user found');
@@ -37,13 +39,18 @@ router.post('/login', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
   try {
-      const newUser = await User.create({
-          username: req.body.username,
-          password: req.body.password,
-      });
+    const newUser = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+
+    req.session.save(() => {
+      req.session.user_id = newUser.id;
+      req.session.logged_in = true;
       res.status(200).json(newUser);
+    });
   } catch (err) {
-      res.status(400).json(err);
+    res.status(400).json(err);
   }
 });
 
